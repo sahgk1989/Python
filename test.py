49ec00b86d35232eeaa0672fa83e48d96ca940f1
dbgsah <gaurav.sah@oracle.com>
2018-05-28 13:35:14 +0530

    Commit date :28May2018

---
+++ test.py
@@ -0,0 +1,6 @@
+x=int(input("Please enter 1st number"))
+y=int(input("Please enter 2nd number"))
+z=int(input("Please enter 3rd number"))
+print("The max of three values is :")
+print(max(x,y,z))
+input("Please press enter to exit")
